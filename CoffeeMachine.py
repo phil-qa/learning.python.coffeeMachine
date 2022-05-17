38d78ca2c29f04666660166abbb053362fde7f75
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-05-17 20:45:05 +0100

    Setup corrections

---
+++ CoffeeMachine.py
@@ -0,0 +1,16 @@
+class CoffeeMachine:
+
+    def __init__(self, water, milk, coffee):
+        self.resources =  {}
+        self.resources['water'] = water
+        self.resources['milk'] = milk
+        self.resources['coffee'] = coffee
+
+    def enough_stock(self, element):
+        if self.resources[element.keys()]:
+        #comment
+
+    def check_availability(self, recipie):
+    #comment
+
+
--- recipes.py
+++ recipes.py
@@ -1,1 +1,23 @@
-recipes = {[]}
+recipes = {
+    "espresso" : {
+        "name" : 'espresso',
+        "price" : 2.20,
+        "water" : .05,
+        "coffee" : .05,
+        "milk" : 0
+    },
+    "cappuchino" : {
+            "name" : 'cappuchino',
+            "price" : 2.90,
+            "water" : .15,
+            "coffee" : .02,
+            "milk" : 0.05
+    },
+    "latte": {
+        "name": 'latte',
+        "price": 3.10,
+        "water": .05,
+        "coffee": .02,
+        "milk": 0.15
+    }
+}
